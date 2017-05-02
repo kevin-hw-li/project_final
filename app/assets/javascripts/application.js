e2deef75968ee991b339460e8c08c0ab1fb38eae
Kevin Li <hoi.li@unswalumni.com>
2017-05-02 13:26:51 +1000

    Added volume to chart and fixed indent of codes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,20 +24,28 @@ var generateChart = function (apiurl) {
   $("svg").remove();
 
   var margin = {top: 20, right: 50, bottom: 30, left: 50},
-          width = 960 - margin.left - margin.right,
-          height = 500 - margin.top - margin.bottom;
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
   var parseDate = d3.timeParse("%Y-%m-%d");
 
   var x = techan.scale.financetime()
-          .range([0, width]);
+    .range([0, width]);
 
   var y = d3.scaleLinear()
-          .range([height, 0]);
+    .range([height, 0]);
+
+  var yVolume = d3.scaleLinear()
+    .range([y(0), y(0.3)]);
 
   var candlestick = techan.plot.candlestick()
-          .xScale(x)
-          .yScale(y);
+    .xScale(x)
+    .yScale(y);
+
+  var volume = techan.plot.volume()
+    .accessor(candlestick.accessor())   // Set the accessor to a candlestick accessor so we get highlighted bars
+    .xScale(x)
+    .yScale(yVolume);
 
   var xAxis = d3.axisBottom(x);
 
@@ -47,120 +55,138 @@ var generateChart = function (apiurl) {
 
   var yRightAxis = d3.axisRight(y);
 
+  var volumeAxis = d3.axisRight(yVolume)
+    .ticks(3)
+    .tickFormat(d3.format(",.3s"));
+
+  var volumeAnnotation = techan.plot.axisannotation()
+    .axis(volumeAxis)
+    .orient("right")
+    .width(35);
+
   var ohlcAnnotation = techan.plot.axisannotation()
-          .axis(yAxis)
-          .orient('left')
-          .format(d3.format(',.2f'));
+    .axis(yAxis)
+    .orient('left')
+    .format(d3.format(',.2f'));
 
   var ohlcRightAnnotation = techan.plot.axisannotation()
-          .axis(yRightAxis)
-          .orient('right')
-          .translate([width, 0]);
+    .axis(yRightAxis)
+    .orient('right')
+    .translate([width, 0]);
 
   var timeAnnotation = techan.plot.axisannotation()
-          .axis(xAxis)
-          .orient('bottom')
-          .format(d3.timeFormat('%Y-%m-%d'))
-          .width(65)
-          .translate([0, height]);
+    .axis(xAxis)
+    .orient('bottom')
+    .format(d3.timeFormat('%Y-%m-%d'))
+    .width(65)
+    .translate([0, height]);
 
   var timeTopAnnotation = techan.plot.axisannotation()
-          .axis(xTopAxis)
-          .orient('top');
+    .axis(xTopAxis)
+    .orient('top');
 
   var crosshair = techan.plot.crosshair()
-          .xScale(x)
-          .yScale(y)
-          .xAnnotation([timeAnnotation, timeTopAnnotation])
-          .yAnnotation([ohlcAnnotation, ohlcRightAnnotation])
-          .on("enter", enter)
-          .on("out", out)
-          .on("move", move);
+    .xScale(x)
+    .yScale(y)
+    .xAnnotation([timeAnnotation, timeTopAnnotation])
+    .yAnnotation([ohlcAnnotation, ohlcRightAnnotation, volumeAnnotation])
+    .on("enter", enter)
+    .on("out", out)
+    .on("move", move);
 
   var svg = d3.select("body").append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   var coordsText = svg.append('text')
-          .style("text-anchor", "end")
-          .attr("class", "coords")
-          .attr("x", width - 5)
-          .attr("y", 15);
+    .style("text-anchor", "end")
+    .attr("class", "coords")
+    .attr("x", width - 5)
+    .attr("y", 15);
 
   d3.csv(apiurl, function(error, data) {
 
-      var accessor = candlestick.accessor();
+    var accessor = candlestick.accessor();
 
-      data = data.map(function(d) {
+    data = data.map(function(d) {
+      return {
+        date: parseDate(d.Date),
+        open: +d.Open,
+        high: +d.High,
+        low: +d.Low,
+        close: +d.Close,
+        volume: +d.Volume
+      };
+    }).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
 
-          return {
-              date: parseDate(d.Date),
-              open: +d.Open,
-              high: +d.High,
-              low: +d.Low,
-              close: +d.Close,
-              volume: +d.Volume
-          };
-      }).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
+    x.domain(data.map(accessor.d));
+    y.domain(techan.scale.plot.ohlc(data, accessor).domain()); // render the time scale
+    yVolume.domain(techan.scale.plot.volume(data).domain());
 
-      x.domain(data.map(accessor.d));
-      y.domain(techan.scale.plot.ohlc(data, accessor).domain()); // render the time scale
+    svg.append("g")
+      .datum(data)
+      .attr("class", "volume")
+      .call(volume);
 
-      svg.append("g")
-              .datum(data)
-              .attr("class", "candlestick")
-              .call(candlestick); // append and render the candlesticks
+    svg.append("g")
+      .datum(data)
+      .attr("class", "candlestick")
+      .call(candlestick); // append and render the candlesticks
 
-      svg.append("g")
-              .attr("class", "x axis")
-              .call(xTopAxis);
+    svg.append("g")
+      .attr("class", "volume axis")
+      .call(volumeAxis);
 
-      svg.append("g")
-              .attr("class", "x axis")
-              .attr("transform", "translate(0," + height + ")")
-              .call(xAxis);
+    svg.append("g")
+      .attr("class", "x axis")
+      .call(xTopAxis);
 
-      svg.append("g")
-              .attr("class", "y axis")
-              .call(yAxis);
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
-      svg.append("g")
-              .attr("class", "y axis")
-              .attr("transform", "translate(" + width + ",0)")
-              .call(yRightAxis);
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
 
-      svg.append('g')
-              .attr("class", "crosshair")
-              .datum({ x: x.domain()[80], y: 67.5 })
-              .call(crosshair)
-              .each(function(d) { move(d); }); // Display the current data
+    svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + width + ",0)")
+      .call(yRightAxis);
+
+    svg.append('g')
+      .attr("class", "crosshair")
+      .datum({ x: x.domain()[80], y: 67.5 })
+      .call(crosshair)
+      .each(function(d) { move(d); }); // Display the current data
 
 
-      var $quote = $("#quote").val();
-      // $("#stock").text($quote.toUpperCase())
-      currentPrice = data[data.length-1].close
-      // $("#price").text(currentPrice + " USD")
+    var $quote = $("#quote").val();
+    // $("#stock").text($quote.toUpperCase())
+    currentPrice = data[data.length-1].close
+    // $("#price").text(currentPrice + " USD")
 
-      svg.append('text')
-              .attr("x", 5)
-              .attr("y", 15)
-              .text($quote.toUpperCase() + " - " + currentPrice + " USD");
+    svg.append('text')
+      .attr("x", 5)
+      .attr("y", 15)
+      .text($quote.toUpperCase() + " - " + currentPrice + " USD");
   });
 
   function enter() {
-      coordsText.style("display", "inline");
+    coordsText.style("display", "inline");
   }
 
   function out() {
-      coordsText.style("display", "none");
+    coordsText.style("display", "none");
   }
 
   function move(coords) {
-      coordsText.text(
-          timeAnnotation.format()(coords.x) + ", " + ohlcAnnotation.format()(coords.y)
-      );
+    coordsText.text(
+        timeAnnotation.format()(coords.x) + ", " + ohlcAnnotation.format()(coords.y)
+    )
   }
 
 };
